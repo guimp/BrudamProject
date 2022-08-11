debf5b800232632670cd43cee05a7d62475c515e
Guilherme de Menezes Pacheco <gui.mp@live.com>
2022-08-11 00:36:09 -0300

    add cliente and pedido controllers

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cliente;
+
+class ClienteController extends Controller
+{
+	public function index()
+	{
+		$cliente = Cliente::all();
+		return response()->json($cliente);
+	}
+
+	public function show($id)
+	{
+		if( Cliente::where('idCliente', $id)->exists() )
+		{
+			$cliente = Cliente::where('idCliente', $id)->get();
+			return  response()->json($cliente);
+		}
+		else
+		{
+			return response()->json([ 'message' => 'Dados do cliente não encontrados', ], 404);
+		}
+	}
+
+	public function store(Request $request)
+	{
+		$cliente = new Cliente;
+
+		$cliente->nome= $request->nome;
+		$cliente->telefone = $request->telefone;
+		$cliente->email = $request->email;
+
+		$retorno = $cliente->save();
+
+		$msgRetorno = $retorno ? "Cliente criado": "Erro ao salvar os dados do cliente";
+
+		return response()->json([
+			"message" => $msgRetorno
+		], 201);
+	}
+}
---
+++ app/Http/Controllers/PedidoController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pedido;
+
+class PedidoController extends Controller
+{
+	public function index()
+	{
+		$pedido = Pedido::all();
+		return response()->json($pedido);
+	}
+
+	public function show($id)
+	{
+		if( Pedido::where('idPedido', $id)->exists() )
+		{
+			$pedido = Pedido::where('idPedido', $id)->get();
+			return  response()->json($pedido);
+		}
+		else
+		{
+			return response()->json([ 'message' => 'Dados do pedido não encontrados', ], 404);
+		}
+	}
+
+	public function store(Request $request)
+	{
+		$pedido = new Pedido;
+
+		$pedido->idPedido= $request->idPedido;
+		$pedido->dataEntrega = $request->dataEntrega;
+		$pedido->valorFrete = $request->valorFrete;
+		$pedido->descricao = $request->descricao;
+		$pedido->idCliente = $request->idCliente;
+
+		$retorno = $pedido->save();
+
+		$msgRetorno = $retorno ? "Pedido criado": "Erro ao criar o pedido";
+
+		return response()->json([
+			"message" => $msgRetorno
+		], 201);
+	}
+
+	public function visualizarPedidos()
+	{
+		$pedido = Pedido::all();
+
+		return view('visualizarPedidos', ['pedido' => $pedido]);
+	}
+}
